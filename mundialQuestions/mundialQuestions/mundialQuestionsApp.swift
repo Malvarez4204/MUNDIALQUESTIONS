adbd3ab0eac71f629df31aed0d4e249be7315ab2
Malvarez4204 <106292326+Malvarez4204@users.noreply.github.com>
2022-07-18 19:57:43 -0500

    SE CARGA ARCHIVO PREGUNTAS

---
+++ mundialQuestions/mundialQuestions/mundialQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mundialQuestionsApp.swift
+//  mundialQuestions
+//
+//  Created by MAC on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct mundialQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
